eafe64ec937efdf20ad40a9f14b7f0062e5ed334
Bennett Miner <minerbj@s.dcsdk12.org>
2021-02-10 09:51:17 -0700

    Completed Activity 5

--- src/Picture.java
+++ src/Picture.java
@@ -95,6 +95,35 @@ public class Picture extends SimplePicture {
 		}
 	}
 
+	public void keepOnlyBlue(){
+		Pixel[][] pixels = this.getPixels2D();
+		for (Pixel[] rowArray : pixels) {
+			for (Pixel pixelObj : rowArray) {
+				pixelObj.setRed(0);
+				pixelObj.setGreen(0);
+			}
+		}
+	}
+
+	public void negate(){
+		Pixel[][] pixels = this.getPixels2D();
+		for (Pixel[] rowArray : pixels) {
+			for (Pixel pixelObj : rowArray) {
+				pixelObj.setRed(0);
+				pixelObj.setBlue(0);
+				pixelObj.setGreen(0);
+			}
+		}
+	}
+
+	public void grayscale(){
+		Pixel[][] pixels = this.getPixels2D();
+		for (Pixel[] rowArray : pixels) {
+			for (Pixel pixelObj : rowArray) {
+			}
+		}
+	}
+
 	/**
 	 * Method that mirrors the picture around a vertical mirror in the center of
 	 * the picture from left to right
